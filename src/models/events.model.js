93419f354561633e3ccff2d7d3f14d8a0ca9c05c
Anthony Mariotti <anthony.mariotti@yahoo.com>
2019-06-06 23:14:27 -0400

    Create Events Mongoose Schema

---
+++ src/models/events.model.js
@@ -0,0 +1,35 @@
+import { Schema, SchemaTypes } from 'mongoose'
+
+const EventsSchema = new Schema(
+    {
+        code: {
+            type: String,
+            minlength: 4,
+            maxlength: 10,
+            unique: true,
+            index: true,
+            trim: true,
+            required: true,
+            uppercase: true,
+        },
+        name: {
+            type: String,
+            minlength: 8,
+            maxlength: 120,
+            required: true,
+            trim: true,
+        },
+        details: {
+            type: String,
+        },
+        open: {
+            type: SchemaTypes.Date,
+            required: true,
+        },
+        close: {
+            type: SchemaTypes.Date,
+            required: true,
+        },
+    },
+    { versionKey: false, collection: 'events', timestamps: true },
+)
