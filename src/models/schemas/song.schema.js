ad6408905484b99ee5c7e971ae91fd61a361fae8
Anthony Mariotti <anthony.mariotti@yahoo.com>
2019-06-20 11:24:39 -0400

    Song Schema Addition Fix

--- src/models/schemas/song.schema.js
+++ src/models/schemas/song.schema.js
@@ -1,20 +1,20 @@
 import { Schema, SchemaTypes } from 'mongoose'
 
-const SongSchema = new Schema({
-    _artistIDs: [
-        {
-            type: SchemaTypes.ObjectId,
-        },
-    ],
-    _albumIDs: [
-        {
-            type: SchemaTypes.ObjectId,
-        },
-    ],
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
+export const SongSchema = new Schema({
+  _artistIDs: [
+    {
+      type: SchemaTypes.ObjectId,
     },
+  ],
+  _albumIDs: [
+    {
+      type: SchemaTypes.ObjectId,
+    },
+  ],
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
 })
