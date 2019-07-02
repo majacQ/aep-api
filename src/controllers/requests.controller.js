ba6420f0ff1117afea1784b27b0726f1167a6e02
Anthony Mariotti <anthony.mariotti@yahoo.com>
2019-07-02 13:32:15 -0400

    All Requests Rotute Update
    TODO: NEEDS DATA POPULATION FOR EVENTS

--- src/controllers/requests.controller.js
+++ src/controllers/requests.controller.js
@@ -1,17 +1,49 @@
 import { Types } from 'mongoose'
-import Requests from '../models/requests.model'
+import chalk from 'chalk'
+import Request from '../models/requests.model'
+import Track from '../models/track.model'
+import Event from '../models/events.model'
 
 export default {
+  // TODO: ERGE EVENT NAME WITH REQUESTS TO COMPRESS DATA OUTPUT
   GetRequests: async (req, res, next) => {
     const { body, query } = req
-    await Requests.paginate(
-      { _workspaceID: Types.ObjectId(req.user._id) },
+    await Request.paginate(
+      { _workspaceID: Types.ObjectId(req.user._workspaceID) },
       {
         limit: query.limit || 5,
         page: query.page || 1,
       },
     )
-      .then((paged) => {
+      .then(async (paged) => {
+        const trackIDs = paged.docs.map((track) => track._trackID)
+        // const eventIDs = paged.docs.map((event) => event._eventID)
+        const tracks = await Track.find({
+          _id: {
+            $in: trackIDs,
+          },
+        })
+        // const events = await Event.find({
+        //   _id: {
+        //     $in: eventIDs,
+        //   },
+        // })
+        paged.docs = paged.docs.map((r) => {
+          const tracksLink = tracks.find((t) =>
+            Types.ObjectId(t._id).equals(r._trackID),
+          )
+          // const eventLink = events.find((e) => {
+          //   Types.ObjectId(e._id).equals(r._eventID)
+          // })
+          return {
+            _id: r._id,
+            _eventID: r._eventID,
+            title: tracksLink.title,
+            explicit: tracksLink.explicit,
+            request_count: r.request_count,
+            played: r.played,
+          }
+        })
         res.status(200).json({
           hasNextPage: paged.hasNextPage,
           hasPrevPage: paged.hasPrevPage,
