5fe07f396335c65483c6689902e09ec6dac882c1
Anthony Mariotti <anthony.mariotti@yahoo.com>
2019-07-31 13:17:31 -0400

    Refactor Teapot Route

--- test/Routes/teapot.route.spec.js
+++ test/Routes/teapot.route.spec.js
@@ -2,22 +2,27 @@ import { assert, should, expect } from 'chai'
 import request from 'supertest'
 import config from '../../src/config'
 import server from '../../src/server'
-const req = request(server)
-const baseURI = `/${config.get('API_VERSION')}`
+const baseURI = `/${config.get('API_VERSION')}/teapot`
 
 describe('Teapot API Route', () => {
   describe('GET /teapot', () => {
-    it('should return status 418', async () => {
-      await req.get(baseURI + '/teapot').then((res) => {
-        const { status } = res
-        expect(status).to.equal(418)
-      })
+    it('should return status 418', (done) => {
+      request(server)
+        .get(baseURI)
+        .expect(418)
+        .end((err, res) => {
+          if (err) return done(err)
+          done()
+        })
     })
-    it('should contain a message', async () => {
-      await req.get(baseURI + '/teapot').then((res) => {
-        const { body } = res
-        expect(body).to.have.property('message')
-      })
+    it('should contain a message', (done) => {
+      request(server)
+        .get(baseURI)
+        .end((err, res) => {
+          if (err) return done(err)
+          expect(res.body).to.haveOwnProperty('message')
+          done()
+        })
     })
   })
 })
